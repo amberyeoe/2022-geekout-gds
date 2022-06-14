7bfec5e20a0292a54d17057fc29900095117cdce
amberyeoe <104883642+amberyeoe@users.noreply.github.com>
2022-06-14 16:08:22 +0800

    amber's very original change

--- backend/src/routes/newMethods.ts
+++ backend/src/routes/newMethods.ts
@@ -1,14 +1,35 @@
 import { Request, Response } from "express";
-// import {todoList} from './methods'
+import {todoList} from './methods'
 
 export async function getTodoById(req: Request, res: Response) {
-    return res.status(501).json({ message: "Not implemented" });
+    const { id } = req.params; 
+    if (id in todoList) {  
+        return res.status(200).json(todoList[id]);
+    } else {
+        return res.status(400).json({ message: "UUID does not exist" });
+    }
 }
 
 export async function deleteTodoById(req: Request, res: Response) {
-    return res.status(501).json({ message: "Not implemented" });
+    const { id } = req.params;
+    if (id in todoList) {
+         delete todoList[id];
+    return res.status(200).send();
+    } else {
+    return res.status(400).json({ message: "UUID does not exist" });
+    }
 }
 
 export async function updateTodoById(req: Request, res: Response) {
-    return res.status(501).json({ message: "Not implemented" });
+    const { id } = req.params;
+    const updatedTodo = req.body;
+    if (updatedTodo.id !== id) {
+        return res.status(409).json({message: "UUID in path and body do not match"});
+    } else if (id in todoList) {
+        todoList[id] = updatedTodo;
+        return res.status(200).send();
+    } else {
+        return res.status(400).json({ message: "UUID does not exist" });
+    }
+        
 }
